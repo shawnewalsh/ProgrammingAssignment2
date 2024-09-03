c8511d03ea0f8b82b8f5373302034649c6f65520
Shawn Walsh <shawn.walsh@gmail.com>
2024-09-03 14:40:36 -0500

    Create makeCacheMatrix.R

---
+++ makeCacheMatrix.R
@@ -0,0 +1,33 @@
+makeCacheMatrix <- function(x = matrix()) {
+  invMatrix <- NULL
+ 
+  set <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+
+  get <- function() x
+  setinverseMatrix <- function(invMatrixerse) invMatrix <<- invMatrixerse
+  getinverseMatrix <- function() invMatrix
+
+  list(set = set, get = get,
+       setinverseMatrix = setinverseMatrix,
+       getinverseMatrix = getinverseMatrix)
+}
+
+
+cacheSolve <- function(x, ...) {
+  invMatrix <- x$getinverseMatrix()
+
+  if(!is.null(invMatrix)) {
+    message("getting cached data")
+    return(invMatrix)
+  }
+
+  data <- x$get()
+  invMatrix <- solve(data, ...)
+
+  x$setinverseMatrix(invMatrix)
+
+  invMatrix
+}
